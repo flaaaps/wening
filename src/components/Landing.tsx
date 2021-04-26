bc995a3a07b0c19dea192c32475abaad715ad8ad
flaaaps <julianwening4@gmail.com>
2021-04-26 22:18:41 +0200

    Move upper section to own "Hero" component

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 
-import peaceMemoji from "../assets/peace.png"
 import Nav from "./Nav"
+import Hero from "./sections/Hero"
 
 interface Props {}
 
@@ -13,24 +13,9 @@ const LandingWrapper = styled.div`
 
 const Landing: React.FC<Props> = () => {
     return (
-        <LandingWrapper className="2xl:h-screen" style={{ paddingBottom: "15vh" }}>
+        <LandingWrapper style={{ paddingBottom: "25vh" }} className="pb-0">
             <Nav />
-            <div
-                style={{ marginTop: "15vh" }}
-                className="flex md:items-center justify-center md:flex-row flex-col items-start w-10/12 mx-auto"
-            >
-                <img src={peaceMemoji} alt="Peace" className="xl:w-64 lg:w-56 md:w-44 w-32 mb-3 mr-5" />
-                <div>
-                    <h1 className="md:text-4xl lg:text-5xl 2xl:text-7xl text-4xl font-heading mb-3">
-                        I’m <strong>Julian Wening</strong>
-                    </h1>
-                    <p className="leading-5 md:leading-5 max-w-lg md:text-lg text-base">
-                        I’m designing and developing modern and professional websites. Whether you need an online shop,
-                        a more personal website or a fully functional web application for your business, I am always at
-                        your service.
-                    </p>
-                </div>
-            </div>
+            <Hero />
         </LandingWrapper>
     )
 }
