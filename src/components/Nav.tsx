100038c1d72b3bbd6b5472281e06cf1d01769872
flaaaps <julianwening4@gmail.com>
2021-05-23 16:43:22 +0200

    Set hrefs of nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,17 @@
 import React from "react"
 import styled from "styled-components"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const Nav: React.FC = () => {
     return (
         <div className="flex justify-end items-end w-10/12 md:w-11/12 m-auto pt-7">
-            <NavButton>Projects</NavButton>
-            <NavButton>Contact</NavButton>
+            <NavButton href="#projects">Projects</NavButton>
+            <NavButton href="#contact">Contact</NavButton>
         </div>
     )
 }
 
-const NavButton: React.FC = ({ children }) => {
+const NavButton: React.FC<{ href: string }> = ({ children, href }) => {
     const Button = styled.button`
         background: linear-gradient(
             95.56deg,
@@ -26,9 +27,11 @@ const NavButton: React.FC = ({ children }) => {
         }
     `
     return (
-        <Button className="lg:text-lg md:px-6 lg:px-7 lg:ml-12 text-md font-heading font-semibold py-1 px-4 rounded-sm md:ml-8 ml-6">
-            {children}
-        </Button>
+        <AnchorLink href={href}>
+            <Button className="lg:text-lg md:px-6 lg:px-7 lg:ml-12 text-md font-heading font-semibold py-1 px-4 rounded-sm md:ml-8 ml-6">
+                {children}
+            </Button>
+        </AnchorLink>
     )
 }
 
