f7d501ed00059ad20d28b0b3c7c416467d929165
flaaaps <julianwening4@gmail.com>
2021-05-24 14:39:34 +0200

    Assign "projects" id to lower element

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -8,10 +8,13 @@ interface Props {}
 
 const Projects = (props: Props) => {
     return (
-        <div id="projects">
+        <div>
             <div className="projects-clip-path mb-4"></div>
             <img src={hello} className="lg:w-36 sm:w-24 w-0 absolute" alt="Peace" />
-            <div className="projects-content pb-72 mt-12 w-11/12 sm:w-1/2 lg:w-3/4 lg:flex lg:justify-between mx-auto box-border">
+            <div
+                id="projects"
+                className="projects-content pb-72 mt-12 w-11/12 sm:w-1/2 lg:w-3/4 lg:flex lg:justify-between mx-auto box-border"
+            >
                 {projectsData.map(project => (
                     <Project project={project} key={project.id} />
                 ))}
