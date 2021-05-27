df008070017cb5bcf94a5cda072405b595140a0f
flaaaps <julianwening4@gmail.com>
2021-05-27 23:22:24 +0200

    NYF: Implement animation component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,24 +1,28 @@
 import React from "react"
 import peaceMemoji from "../../assets/peace.png"
 
+import AnimateWhenVisible from "../AnimateWhenVisible"
+
 const Hero: React.FC = () => {
     return (
-        <div
-            style={{ marginTop: "15vh" }}
-            className="mt-0 flex md:items-center justify-center md:flex-row flex-col items-start w-10/12 mx-auto"
-        >
-            <img src={peaceMemoji} alt="Peace" className="xl:w-60 lg:w-56 md:w-44 w-32 mb-3 mr-5" />
-            <div>
-                <h1 className="md:text-4xl lg:text-5xl 2xl:text-6xl text-4xl font-heading mb-4 tracking-tight sm:tracking-normal">
-                    I’m <strong className="ml-1">Julian Wening</strong>
-                </h1>
-                <p className="leading-5 md:leading-5 max-w-lg xl:max-w-xl md:text-lg xl:leading-6 xl:text-xl font-normal text-base">
-                    I’m designing and developing modern and professional websites. Whether you need an online shop, a
-                    more personal website or a fully functional web application for your business, I am always at your
-                    service.
-                </p>
+        <AnimateWhenVisible>
+            <div
+                style={{ marginTop: "15vh" }}
+                className="mt-0 flex md:items-center justify-center md:flex-row flex-col items-start w-10/12 mx-auto"
+            >
+                <img src={peaceMemoji} alt="Peace" className="xl:w-60 lg:w-56 md:w-44 w-32 mb-3 mr-5" />
+                <div>
+                    <h1 className="md:text-4xl lg:text-5xl 2xl:text-6xl text-4xl font-heading mb-4 tracking-tight sm:tracking-normal">
+                        I’m <strong className="ml-1">Julian Wening</strong>
+                    </h1>
+                    <p className="leading-5 md:leading-5 max-w-lg xl:max-w-xl md:text-lg xl:leading-6 xl:text-xl font-normal text-base">
+                        I’m designing and developing modern and professional websites. Whether you need an online shop,
+                        a more personal website or a fully functional web application for your business, I am always at
+                        your service.
+                    </p>
+                </div>
             </div>
-        </div>
+        </AnimateWhenVisible>
     )
 }
 
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -2,15 +2,17 @@ import React from "react"
 import hello from "../../assets/hello.png"
 import Project from "../Project"
 
+import AnimateWhenVisible from "../AnimateWhenVisible"
+
 import { data as projectsData } from "../../data/projects"
 
-interface Props {}
-
-const Projects = (props: Props) => {
+const Projects: React.FC = () => {
     return (
         <div>
             <div className="projects-clip-path mb-4"></div>
-            <img src={hello} className="lg:w-36 sm:w-24 w-0 absolute" alt="Peace" />
+            <AnimateWhenVisible>
+                <img src={hello} className="lg:w-36 sm:w-24 w-0 absolute" alt="Peace" />
+            </AnimateWhenVisible>
             <div
                 id="projects"
                 className="projects-content pb-72 mt-12 w-11/12 sm:w-1/2 lg:w-3/4 lg:flex lg:justify-between xl:w-4/6 mx-auto box-border"
