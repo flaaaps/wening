2918ba935771d1e895cee09e15c596ee5a83868d
flaaaps <julianwening4@gmail.com>
2021-05-28 09:52:27 +0200

    Animate each project with some delay

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,6 +4,7 @@ import { motion, Variants } from "framer-motion"
 import { isMobile } from "react-device-detect"
 
 import { Project as IProject } from "../data/projects"
+import AnimateWhenVisible from "./AnimateWhenVisible"
 
 interface Props {
     project: IProject
@@ -26,62 +27,69 @@ const overlayMotion: Variants = {
 const Project: React.FC<Props> = ({ project }) => {
     const [isFocused, setFocused] = useState(false)
     return (
-        <motion.div
+        <AnimateWhenVisible
             className="w-full shadow-xl my-12 md:mx-6 md:my-5 relative overflow-hidden focus:outline-none"
-            whileHover="hover"
-            onHoverStart={() => !isMobile && setFocused(true)}
-            onHoverEnd={() => !isMobile && setFocused(false)}
-            tabIndex={-1}
-            onFocus={() => (isMobile || process.env.NODE_ENV !== "development") && setFocused(true)}
-            onBlur={() => (isMobile || process.env.NODE_ENV !== "development") && setFocused(false)}
-            initial="rest"
-            animate={isFocused ? "hover" : "rest"}
+            variants={{
+                visible: { opacity: 1, y: 0, transition: { delay: 0.25 * project.id } },
+                hidden: { opacity: 0, y: 100 },
+            }}
         >
-            {/* <a href={project.links[0].url}>Hello</a> */}
-            <img
-                style={{ filter: isFocused ? "blur(5px)" : "none" }}
-                src={project.previewImg}
-                className="w-full"
-                alt=""
-            />
-            <motion.div className={classNames("pj-overlay w-full h-full absolute")} variants={overlayMotion}>
-                <div className="py-3 px-6 lg:py-4 lg:px-8 sep flex flex-col justify-center h-full">
-                    <div className="pj-tags hidden md:block my-3 mb-4">
-                        {project.tags.map(tag => (
-                            <span
-                                key={tag}
-                                className={classNames(
-                                    "tag-color-" + tag.toLowerCase(),
-                                    "mr-4 py-1 px-3 text-xs lg:text-sm"
-                                )}
+            <motion.div
+                whileHover="hover"
+                onHoverStart={() => !isMobile && setFocused(true)}
+                onHoverEnd={() => !isMobile && setFocused(false)}
+                tabIndex={-1}
+                onFocus={() => (isMobile || process.env.NODE_ENV !== "development") && setFocused(true)}
+                onBlur={() => (isMobile || process.env.NODE_ENV !== "development") && setFocused(false)}
+                initial="rest"
+                animate={isFocused ? "hover" : "rest"}
+            >
+                {/* <a href={project.links[0].url}>Hello</a> */}
+                <img
+                    style={{ filter: isFocused ? "blur(5px)" : "none" }}
+                    src={project.previewImg}
+                    className="w-full"
+                    alt=""
+                />
+                <motion.div className={classNames("pj-overlay w-full h-full absolute")} variants={overlayMotion}>
+                    <div className="py-3 px-6 lg:py-4 lg:px-8 sep flex flex-col justify-center h-full">
+                        <div className="pj-tags hidden md:block my-3 mb-4">
+                            {project.tags.map(tag => (
+                                <span
+                                    key={tag}
+                                    className={classNames(
+                                        "tag-color-" + tag.toLowerCase(),
+                                        "mr-4 py-1 px-3 text-xs lg:text-sm"
+                                    )}
+                                >
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                        <div className="pj-text-content">
+                            <h1
+                                className="text-2xl lg:text-3xl font-bold font-heading"
+                                style={{ color: project.title.color }}
                             >
-                                {tag}
-                            </span>
-                        ))}
+                                {project.title.content}
+                            </h1>
+                        </div>
+                        <p className="mt-1 text-white font-body tracking-tighter font-normal text-base lg:text-lg leading-5">
+                            {project.description}
+                        </p>
+                        <div className="pj-link-wrapper mt-4 md:mt-8">
+                            {project.links.map(link => (
+                                <span key={link.type} className="mr-4">
+                                    <a href={link.url} target="_blank" rel="noreferrer">
+                                        <img className="inline-block w-5 md:w-6" src={link.iconUrl} alt="" />
+                                    </a>
+                                </span>
+                            ))}
+                        </div>
                     </div>
-                    <div className="pj-text-content">
-                        <h1
-                            className="text-2xl lg:text-3xl font-bold font-heading"
-                            style={{ color: project.title.color }}
-                        >
-                            {project.title.content}
-                        </h1>
-                    </div>
-                    <p className="mt-1 text-white font-body tracking-tighter font-normal text-base lg:text-lg leading-5">
-                        {project.description}
-                    </p>
-                    <div className="pj-link-wrapper mt-4 md:mt-8">
-                        {project.links.map(link => (
-                            <span key={link.type} className="mr-4">
-                                <a href={link.url} target="_blank" rel="noreferrer">
-                                    <img className="inline-block w-5 md:w-6" src={link.iconUrl} alt="" />
-                                </a>
-                            </span>
-                        ))}
-                    </div>
-                </div>
+                </motion.div>
             </motion.div>
-        </motion.div>
+        </AnimateWhenVisible>
     )
 }
 
