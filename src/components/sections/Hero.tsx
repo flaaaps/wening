d20ba40d920d8f421392614fe025dcac7664c3f4
flaaaps <julianwening4@gmail.com>
2021-05-28 09:52:44 +0200

    Improve hero animations

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,24 +5,34 @@ import AnimateWhenVisible from "../AnimateWhenVisible"
 
 const Hero: React.FC = () => {
     return (
-        <AnimateWhenVisible>
-            <div
-                style={{ marginTop: "15vh" }}
-                className="mt-0 flex md:items-center justify-center md:flex-row flex-col items-start w-10/12 mx-auto"
+        <div
+            style={{ marginTop: "15vh" }}
+            className="mt-0 flex md:items-center justify-center md:flex-row flex-col items-start w-10/12 mx-auto"
+        >
+            <AnimateWhenVisible
+                variants={{
+                    visible: { opacity: 1, scale: 1, transition: { type: "spring", bounce: 0.5 } },
+                    hidden: { opacity: 0, scale: 0.5 },
+                }}
             >
                 <img src={peaceMemoji} alt="Peace" className="xl:w-60 lg:w-56 md:w-44 w-32 mb-3 mr-5" />
-                <div>
-                    <h1 className="md:text-4xl lg:text-5xl 2xl:text-6xl text-4xl font-heading mb-4 tracking-tight sm:tracking-normal">
-                        I’m <strong className="ml-1">Julian Wening</strong>
-                    </h1>
-                    <p className="leading-5 md:leading-5 max-w-lg xl:max-w-xl md:text-lg xl:leading-6 xl:text-xl font-normal text-base">
-                        I’m designing and developing modern and professional websites. Whether you need an online shop,
-                        a more personal website or a fully functional web application for your business, I am always at
-                        your service.
-                    </p>
-                </div>
-            </div>
-        </AnimateWhenVisible>
+            </AnimateWhenVisible>
+            <AnimateWhenVisible
+                variants={{
+                    visible: { opacity: 1 },
+                    hidden: { opacity: 0 },
+                }}
+            >
+                <h1 className="md:text-4xl lg:text-5xl 2xl:text-6xl text-4xl font-heading mb-4 tracking-tight sm:tracking-normal">
+                    I’m <strong className="ml-1">Julian Wening</strong>
+                </h1>
+                <p className="leading-5 md:leading-5 max-w-lg xl:max-w-xl md:text-lg xl:leading-6 xl:text-xl font-normal text-base">
+                    I’m designing and developing modern and professional websites. Whether you need an online shop, a
+                    more personal website or a fully functional web application for your business, I am always at your
+                    service.
+                </p>
+            </AnimateWhenVisible>
+        </div>
     )
 }
 
